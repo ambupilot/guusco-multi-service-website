b78332b6aba494db7a5959ad896f8208b4b8c285
Martijn Kerssing <martijn@kerssing.nl>
2026-08-13 21:22:06 +0200

    feat: connect site shell

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist } from "next/font/google";
+import { SiteFooter } from "@/components/layout/SiteFooter";
+import { SiteHeader } from "@/components/layout/SiteHeader";
 import "@/styles/globals.scss";
 
 const geistSans = Geist({
@@ -8,18 +10,23 @@ const geistSans = Geist({
 });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://guusco.nl"),
   title: {
-    default: "GuusCo Multiservice",
+    default: "GuusCo Multiservice | Klus- en onderhoudsbedrijf Nijmegen",
     template: "%s | GuusCo Multiservice",
   },
   description:
-    "GuusCo Multiservice voor timmerwerk, elektra, sanitair, montage, reparaties en kleine verbouwingen in Nijmegen en omgeving.",
+    "GuusCo Multiservice voor timmerwerk, elektra, sanitair, tegelwerk, montage, reparaties en kleine verbouwingen in Nijmegen en omgeving.",
 };
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html lang="nl" className={geistSans.variable}>
-      <body>{children}</body>
+      <body>
+        <SiteHeader />
+        {children}
+        <SiteFooter />
+      </body>
     </html>
   );
 }
